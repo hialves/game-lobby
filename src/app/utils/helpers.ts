a7ed29e738def7ec1b1b7552e2aa05543f649b91
hialves <hiagotnt1@gmail.com>
2020-12-10 03:52:50 -0300

    add helpers functions

--- src/app/utils/helpers.ts
+++ src/app/utils/helpers.ts
@@ -13,3 +13,15 @@ export const compareHash = async (password: string, hash: string) => {
 export const capitalizeFirst = (value: string) => {
   return value.charAt(0).toUpperCase() + value.slice(1)
 }
+
+export const hasSymbols = (str: string) => {
+  return str.match(/^[^a-zA-Z0-9]+$/) ? true : false
+}
+
+export const isValid = (val: any) => {
+  if (val !== '' && val !== undefined && val !== null) {
+    return true
+  }
+
+  return false
+}
